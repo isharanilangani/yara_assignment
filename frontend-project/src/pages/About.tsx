2f6b4e71dc3fe1242f13a84ba9a54b77739d182b
isharanilangani <98374135+isharanilangani@users.noreply.github.com>
2025-05-05 20:53:23 +0530

    style:add style for about page

--- frontend-project/src/pages/About.tsx
+++ frontend-project/src/pages/About.tsx
@@ -1,11 +1,22 @@
 const About: React.FC = () => {
-    return (
-      <div className="container mt-4">
-        <h2>About This App</h2>
-        <p>This application demonstrates fetching and displaying posts using React and TypeScript.</p>
+  return (
+    <div className="container mt-5">
+      <div className="row align-items-center">
+        {/* Background image section */}
+        <div className="col-md-6 text-center mb-4 mb-md-0">
+            <div className="post-image w-100"></div>
+          </div>
+
+        {/* Text content section */}
+        <div className="col-md-6 post-details">
+          <h2 className="fw-bold text-dark">About This App</h2>
+          <p className="text-secondary mt-3">
+            This application demonstrates fetching and displaying posts using React and TypeScript.
+          </p>
+        </div>
       </div>
-    );
-  };
-  
-  export default About;
-  
+    </div>
+  );
+};
+
+export default About;

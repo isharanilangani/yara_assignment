b4a5ba12df1cb10590b37d10cf021c0d7e7291a0
isharanilangani <98374135+isharanilangani@users.noreply.github.com>
2025-05-06 19:33:15 +0530

    style: add style for mobile view background image style

--- frontend-project/src/pages/Home.tsx
+++ frontend-project/src/pages/Home.tsx
@@ -1,14 +1,30 @@
 import React from "react";
 import PostsFetch from "../components/PostsFetch";
+import "../styles/global.css"; // Make sure your styles are imported
 
 const Home: React.FC = () => {
   return (
     <div className="position-relative">
       <section className="pt-5 bg-white" style={{ marginTop: "80px" }}>
         <div className="container">
-          <div className="row align-items-center">
-            <div className="col-md-6 text-start">
-              <h1 className="display-4 fw-bold text-dark">
+          <div className="row align-items-center text-center text-md-start">
+            {/* Image on top for mobile, right on desktop */}
+            <div className="col-md-6 order-1 order-md-2 cloud-shape-container">
+              <div className="cloud-shape mx-auto"></div>
+
+              {/* SVG definition for clip-path */}
+              <svg width="0" height="0">
+                <defs>
+                  <clipPath id="cloud-shape" clipPathUnits="objectBoundingBox">
+                    <path d="M0.4,0 C0.8,0.1 0.8,0 0.8,0.1 C0.8,0.2 1,0.6 0.8,0.8 C0.8,1 0.5,1 0.4,0.9 C0.2,0.8 0.2,0.7 0.2,0.6 C0.1,0.5 0,0.4 0.3,0.1 Z" />
+                  </clipPath>
+                </defs>
+              </svg>
+            </div>
+
+            {/* Text below image on mobile, left on desktop */}
+            <div className="col-md-6 order-2 order-md-1">
+              <h1 className="display-5 fw-bold text-dark">
                 Innovative Solutions for Modern Businesses
               </h1>
               <p className="lead text-secondary mt-3">
@@ -22,25 +38,11 @@ const Home: React.FC = () => {
                 Explore
               </a>
             </div>
-
-            {/* Oval Image - Right */}
-            <div className="col-md-6 cloud-shape-container">
-              <div className="cloud-shape mx-auto"></div>
-
-              {/* SVG with Cloud Shape Definition */}
-              <svg width="0" height="0">
-                <defs>
-                  <clipPath id="cloud-shape" clipPathUnits="objectBoundingBox">
-                    <path d="M0.4,0 C0.8,0.1 0.8,0 0.8,0.1 C0.8,0.2 1,0.6 0.8,0.8 C0.8,1 0.5,1 0.4,0.9 C0.2,0.8 0.2,0.7 0.2,0.6 C0.1,0.5 0,0.4 0.3,0.1 Z" />
-                  </clipPath>
-                </defs>
-              </svg>
-            </div>
           </div>
         </div>
       </section>
 
-      {/* Posts */}
+      {/* Posts section */}
       <div id="posts" className="container mt-5">
         <PostsFetch />
       </div>

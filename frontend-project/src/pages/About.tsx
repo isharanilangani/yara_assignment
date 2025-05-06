1c12ff98041a8ccd75eea868fa1d01dca8d9623e
isharanilangani <98374135+isharanilangani@users.noreply.github.com>
2025-05-06 16:21:41 +0530

    style(about): add style for about page

--- frontend-project/src/pages/About.tsx
+++ frontend-project/src/pages/About.tsx
@@ -1,21 +1,26 @@
+import React from "react";
+import AboutContent from "../components/AboutContent";
+import { Link } from "react-router-dom";
+import "../styles/global.css";
+
 const About: React.FC = () => {
   return (
-    <div className="container mt-5">
-      <div className="row align-items-center">
-        {/* Background image section */}
-        <div className="col-md-6 text-center mb-4 mb-md-0">
-            <div className="post-image w-100"></div>
-          </div>
-
-        {/* Text content section */}
-        <div className="col-md-6 post-details">
-          <h2 className="fw-bold text-dark">About This App</h2>
-          <p className="text-secondary mt-3">
-            This application demonstrates fetching and displaying posts using React and TypeScript.
-          </p>
+    <>
+      <div className="post-image position-relative">
+        <div className="header-navigation">
+          <Link to="/" className="home-link me-1">
+            <i className="bi bi-house-door" style={{ fontSize: "20px" }}></i>{" "}
+            Home
+          </Link>
+          <span className="text-white">&nbsp;&gt; About</span>
         </div>
       </div>
-    </div>
+
+      {/* Text content section */}
+      <div className="d-flex justify-content-center align-items-center">
+        <AboutContent />
+      </div>
+    </>
   );
 };
 

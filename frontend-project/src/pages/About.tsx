b306b1bf410186a8368d7eb7e76da6cff8f0c8cc
isharanilangani <98374135+isharanilangani@users.noreply.github.com>
2025-05-06 22:51:46 +0530

    optimize: cleanup code

--- frontend-project/src/pages/About.tsx
+++ frontend-project/src/pages/About.tsx
@@ -6,20 +6,18 @@ import "../styles/global.css";
 const About: React.FC = () => {
   return (
     <>
+      {/* Image Section */}
       <div className="post-image position-relative">
         <div className="header-navigation position-absolute top-0 start-0 m-3 fw-bold">
           <Link to="/" className="home-link me-1">
-            <i className="bi bi-house-door fw-bold" style={{ fontSize: "20px" }}></i>{" "}
+            <i className="bi bi-house-door fw-bold icon-home"></i>{" "}
           </Link>
           <span className="text-white">&nbsp;&gt; About</span>
         </div>
       </div>
 
-      {/* Text content section */}
-      <div
-        className="container"
-        style={{ marginTop: "-70px", zIndex: 2, position: "relative" }}
-      >
+      {/* Text section */}
+      <div className="container container-margin">
         <AboutContent />
       </div>
     </>
